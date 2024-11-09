259ceec47219ac446559d3fb1c1f3d32cd60afa0
898ronin <greenkappa16@gmail.com>
2024-11-08 22:49:21 -0300

    aaaa

--- assets/script.js
+++ assets/script.js
@@ -155,7 +155,7 @@ estilo.textContent = `
         50% { transform: translateY(-10px); }
     }
     span {
-        font-size: 20px;
+        font-size: 15px;
         font-weight: bold;
         margin: 0 2px;
     }
